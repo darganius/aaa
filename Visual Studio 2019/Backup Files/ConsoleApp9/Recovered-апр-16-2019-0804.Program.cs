43749d2f75fbbd5e262f6c0d92f5429eb3c823ff
darganius <rahahha@gmail.com>
2019-09-23 20:33:56 +0600

    bbyb

---
+++ Visual Studio 2019/Backup Files/ConsoleApp9/Recovered-апр-16-2019-0804.Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int f = 0;
+            int x = Convert.ToInt32(Console.ReadLine());
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            int dx = Convert.ToInt32(Console.ReadLine());
+            for (int i=0;i<=10; i+=dx)
+            {
+                if (c < 0 && a != 0) f = -a * (x * x);
+                else if (c > 0 && a == 0) f = (a - x) / (c * x);
+                else f = x / c;
+                Console.WriteLine(f);
+                Console.ReadKey();
+            }
+        }
+    }
+}
